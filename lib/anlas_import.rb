fbd73f4841b023172ddd8065da7b739c02790a04
Tyralion <piliaiev@gmail.com>
2012-06-04 14:01:36 +0600

    Правки. Убрана привязка к Railtie.

--- lib/anlas_import.rb
+++ lib/anlas_import.rb
@@ -22,16 +22,6 @@ module AnlasImport
       puts "This method must be redefined"
     end # self.run
 
-    def self.can_start?
-
-      return false if defined?(::IRB)
-      return false if defined?(::Rake)
-      return false unless defined?(::Rails)
-      return false if ::Rails.env.to_s != "production"
-      true
-
-    end # self.can_start?
-
   end # Base
 
 end # AnlasImport
@@ -50,5 +40,3 @@ require 'anlas_import/mailer'
 require 'anlas_import/xml_parser'
 require 'anlas_import/worker'
 require 'anlas_import/manager'
-
-require 'anlas_import/railtie' if defined?(::Rails)
