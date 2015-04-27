cf661e78bba07ff559281667317514a7b80bfc32
Ivan Pilyaev <piliaiev@gmail.com>
2015-04-27 11:05:51 +0500

    Мелкие правки.

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -4,19 +4,17 @@ class ImportController < ApplicationController
   unloadable
 
   before_filter :auth
-  skip_before_filter :verify_authenticity_token, :only => :save_file
+  skip_before_filter :verify_authenticity_token
 
   def index
 
     case params[:mode]
-    when 'checkauth'
-      render(:text => "success\nimport_1c\n#{rand(9999)}", :layout => false) and return
-    when 'init'
-      render(:text => "zip=yes\nfile_limit=999999999", :layout => false) and return
-    when 'import'
-      render(:text => "success", :layout => false) and return
-    else
-      render(:text => "failure", :layout => false) and return
+      when 'checkauth'
+        render(:text => "success\nimport_1c\n#{rand(9999)}", :layout => false) and return
+      when 'init'
+        render(:text => "zip=yes\nfile_limit=999999999", :layout => false) and return
+      else
+        render(:text => "success", :layout => false) and return
     end
 
   end # index
